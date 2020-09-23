8ab16c39998a429fd0b8a6080ba0435eb73a90e0
Ann-Marie <anniedillier@gmail.com>
2020-09-22 20:31:02 -0700

    potions and test

--- __tests__/random.test.js
+++
@@ -1,7 +0,0 @@
-const { enhanceUnexpectedTokenMessage } = require('@jest/transform/build/enhanceUnexpectedTokenMessage');
-const { TestResult } = require('@jest/types');
-const checkIfEqual = require('../lib/random.js');
-
-TestResult('checks if 10 is equal to 10', () => {
-    enhanceUnexpectedTokenMessage(checkIfEqual(10, 10)).toBe(true);
-});
